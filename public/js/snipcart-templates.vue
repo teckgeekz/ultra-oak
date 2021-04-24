6c4a1ab8263b6a23752764f983cd10b9c70c3243
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2021-04-24 23:19:05 +0530

    Update snipcart-templates.vue

--- public/js/snipcart-templates.vue
+++ public/js/snipcart-templates.vue
@@ -1,16 +1,3 @@
 <div hidden id="snipcart" data-api-key="__SNIPCART_API_KEY__">
-<billing section="bottom">
-    <fieldset class="snipcart-form__set">
-        <div class="snipcart-form__field">
-            <div class="snipcart-form__field">
-                
-                <snipcart-label class="snipcart__font--tiny" for="phone">Phone #</snipcart-label>
-                <snipcart-input name="phone"></snipcart-input>
-                    
-                
-            </div>
-        </div>
-    </fieldset>
-</billing>
 
 </div>
