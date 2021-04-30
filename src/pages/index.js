5eefaa4e4e6f68827ca60f7a25c4b9cd648faaf1
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2021-04-30 06:23:39 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { sourcebitDataClient } from 'sourcebit-target-next';
 
 /**
@@ -13,6 +14,23 @@ export async function getStaticProps({ params }) {
     console.log('Page [index] getStaticProps, params: ', params);
     const props = await sourcebitDataClient.getStaticPropsForPageAtPath('/');
     return { props };
+    <head>
+        <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1188748351555027');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1188748351555027&ev=PageView&noscript=1"
+/></noscript>
+     </head>
 }
 
 export default Page;
